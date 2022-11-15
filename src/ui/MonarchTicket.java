777e3ede75f09adc112ab3bc3ae64d3b0b5a1722
andrei-r.ionescu <andrei-r.ionescu@student.upt.ro>
2022-11-15 20:30:05 +0200

    THE FINAL COMMIT: Monarch Ticket final GUI(like Wizzair Ticket). Ryanair Ticket final GUI(like Wizzair Ticket).

--- src/ui/MonarchTicket.java
+++ src/ui/MonarchTicket.java
@@ -3,6 +3,8 @@ package ui;
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import static java.awt.Color.blue;
 import static java.awt.Color.white;
@@ -10,9 +12,22 @@ import static java.awt.Color.white;
 public class MonarchTicket {
     static JFrame monarchFrame = new JFrame();
     static JPanel monarchPanel = new JPanel();
-    static JLabel barcelonaTicket2 = new JLabel("Barcelona(E) - EL PRAT");
-    static JLabel londonTicket = new JLabel("London(UK) - LUTON");
-    static JLabel manchesterTicket = new JLabel("Manchester(UK) - Airport");
+    static JLabel barcelonaTicket2 = new JLabel("Barcelona(E) - EL PRAT - 127$");
+    static JLabel londonTicket = new JLabel("London(UK) - LUTON - 125$");
+    static JLabel manchesterTicket = new JLabel("Manchester(UK) - Airport - 117$");
+
+    //for ticket ui
+    static JLabel nameMonarch = new JLabel("NAME:");
+    static JTextField nameTextMonarch = new JTextField();
+    static JLabel surnameMonarch = new JLabel("SURNAME:");
+    static JTextField surnameTextMonarch = new JTextField();
+    static JLabel emailMonarch = new JLabel("E-MAIL:");
+    static JTextField emailTextMonarch = new JTextField();
+    static JLabel phoneMonarch = new JLabel("PHONE:");
+    static JTextField phoneTextMonarch = new JTextField();
+    static JLabel companyMonarchMessage = new JLabel("#VisitUKwithMonarch:)");
+    static JButton monarchTicketButton = new JButton();
+    //-----------------------
 
 
     public void setMonarchTicket(){
@@ -24,25 +39,91 @@ public class MonarchTicket {
         monarchPanel.setBackground(blue);
         monarchPanel.setLayout(null);
 
+        nameMonarch.setBounds(180, 140, 550, 100);
+        nameMonarch.setFont(new Font("Serif", Font.BOLD, 34));
+        nameMonarch.setForeground(Color.orange);
+        monarchPanel.add(nameMonarch);
+
+        nameTextMonarch.setBounds(300, 161, 350, 60);
+        nameTextMonarch.setFont(new Font("Serif", Font.BOLD, 34));
+        nameTextMonarch.setBackground(white);
+        nameTextMonarch.setForeground(blue);
+        monarchPanel.add(nameTextMonarch);
+
+        surnameMonarch.setBounds(180, 240, 550, 100);
+        surnameMonarch.setFont(new Font("Serif", Font.BOLD, 34));
+        surnameMonarch.setForeground(Color.orange);
+        monarchPanel.add(surnameMonarch);
+
+        surnameTextMonarch.setBounds(370, 261, 350, 60);
+        surnameTextMonarch.setFont(new Font("Serif", Font.BOLD, 34));
+        surnameTextMonarch.setBackground(white);
+        surnameTextMonarch.setForeground(blue);
+        monarchPanel.add(surnameTextMonarch);
+
+        emailMonarch.setBounds(180, 340, 550, 100);
+        emailMonarch.setFont(new Font("Serif", Font.BOLD, 34));
+        emailMonarch.setForeground(Color.orange);
+        monarchPanel.add(emailMonarch);
+
+        emailTextMonarch.setBounds(320, 361, 350, 60);
+        emailTextMonarch.setFont(new Font("Serif", Font.BOLD, 34));
+        emailTextMonarch.setBackground(white);
+        emailTextMonarch.setForeground(blue);
+        monarchPanel.add(emailTextMonarch);
+
+        phoneMonarch.setBounds(180, 440, 550, 100);
+        phoneMonarch.setFont(new Font("Serif", Font.BOLD, 34));
+        phoneMonarch.setForeground(Color.orange);
+        monarchPanel.add(phoneMonarch);
+
+        phoneTextMonarch.setBounds(320, 461, 350, 60);
+        phoneTextMonarch.setFont(new Font("Serif", Font.BOLD, 34));
+        phoneTextMonarch.setBackground(white);
+        phoneTextMonarch.setForeground(blue);
+        monarchPanel.add(phoneTextMonarch);
+
+        companyMonarchMessage.setBounds(180, 540, 550, 100);
+        companyMonarchMessage.setFont(new Font("Serif", Font.BOLD, 27));
+        companyMonarchMessage.setForeground(Color.orange);
+        monarchPanel.add(companyMonarchMessage);
+
+        monarchTicketButton.setBounds(900, 355, 300, 110);
+        monarchTicketButton.setText("Order your Monarch Ticket!");
+        monarchTicketButton.setFont(new Font("Serif", Font.BOLD, 21));
+        monarchPanel.add(monarchTicketButton);
+
+
+        monarchTicketButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String emailM = emailTextMonarch.getText();
+
+                JOptionPane.showMessageDialog(null, "Thank you for flying with us!We sent you a confirmation via e-mail:" + emailM + "!");
+                return;
+            }
+        });
+
+
+
         monarchFrame.setVisible(true);
     }
 
     public void setBarcelonaTicket2(){
-        barcelonaTicket2.setBounds(470, 25, 550, 100);
+        barcelonaTicket2.setBounds(470, 25, 750, 100);
         barcelonaTicket2.setFont(new Font("Serif", Font.BOLD, 43));
         barcelonaTicket2.setForeground(Color.orange);
         monarchPanel.add(barcelonaTicket2);
     }
 
     public void setLondonTicket(){
-        londonTicket.setBounds(470, 25, 550, 100);
+        londonTicket.setBounds(470, 25, 750, 100);
         londonTicket.setFont(new Font("Serif", Font.BOLD, 43));
         londonTicket.setForeground(Color.orange);
         monarchPanel.add(londonTicket);
     }
 
     public void setManchesterTicket(){
-        manchesterTicket.setBounds(470, 25, 550, 100);
+        manchesterTicket.setBounds(470, 25, 750, 100);
         manchesterTicket.setFont(new Font("Serif", Font.BOLD, 43));
         manchesterTicket.setForeground(Color.orange);
         monarchPanel.add(manchesterTicket);
--- src/ui/RyanairTicket.java
+++ src/ui/RyanairTicket.java
@@ -2,16 +2,32 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import static java.awt.Color.white;
-import static java.awt.Color.yellow;
+import static java.awt.Color.*;
+import static java.awt.Color.blue;
 
 public class RyanairTicket {
     static JFrame ryanairFrame = new JFrame();
     static JPanel ryanairPanel = new JPanel();
-    static JLabel barcelonaTicket3 = new JLabel("Barcelona(E) - EL PRAT");
-    static JLabel londonTicket2 = new JLabel("London(UK) - LUTON");
-    static JLabel manchesterTicket2 = new JLabel("Manchester(UK) - Airport");
+    static JLabel barcelonaTicket3 = new JLabel("Barcelona(E) - EL PRAT - 130$");
+    static JLabel londonTicket2 = new JLabel("London(UK) - LUTON - 122$");
+    static JLabel manchesterTicket2 = new JLabel("Manchester(UK) - Airport - 111$");
+
+    //for ticket ui
+    static JLabel nameRyan = new JLabel("NAME:");
+    static JTextField nameTextRyan = new JTextField();
+    static JLabel surnameRyan = new JLabel("SURNAME:");
+    static JTextField surnameTextRyan = new JTextField();
+    static JLabel emailRyan = new JLabel("E-MAIL:");
+    static JTextField emailTextRyan = new JTextField();
+    static JLabel phoneRyan = new JLabel("PHONE:");
+    static JTextField phoneTextRyan = new JTextField();
+    static JLabel companyRyanMessage = new JLabel("We are trying to offer yourself the best prices!");
+    static JButton ryanTicketButton = new JButton();
+    //-----------------------
+
 
     public void setRyanairTicket(){
         ryanairFrame.setTitle("RYANAIR Ticket");
@@ -22,25 +38,90 @@ public class RyanairTicket {
         ryanairPanel.setBackground(yellow);
         ryanairPanel.setLayout(null);
 
+        nameRyan.setBounds(180, 140, 550, 100);
+        nameRyan.setFont(new Font("Serif", Font.BOLD, 34));
+        nameRyan.setForeground(Color.blue);
+        ryanairPanel.add(nameRyan);
+
+        nameTextRyan.setBounds(300, 161, 350, 60);
+        nameTextRyan.setFont(new Font("Serif", Font.BOLD, 34));
+        nameTextRyan.setBackground(white);
+        nameTextRyan.setForeground(blue);
+        ryanairPanel.add(nameTextRyan);
+
+        surnameRyan.setBounds(180, 240, 550, 100);
+        surnameRyan.setFont(new Font("Serif", Font.BOLD, 34));
+        surnameRyan.setForeground(Color.blue);
+        ryanairPanel.add(surnameRyan);
+
+        surnameTextRyan.setBounds(370, 261, 350, 60);
+        surnameTextRyan.setFont(new Font("Serif", Font.BOLD, 34));
+        surnameTextRyan.setBackground(white);
+        surnameTextRyan.setForeground(blue);
+        ryanairPanel.add(surnameTextRyan);
+
+        emailRyan.setBounds(180, 340, 550, 100);
+        emailRyan.setFont(new Font("Serif", Font.BOLD, 34));
+        emailRyan.setForeground(Color.blue);
+        ryanairPanel.add(emailRyan);
+
+        emailTextRyan.setBounds(320, 361, 350, 60);
+        emailTextRyan.setFont(new Font("Serif", Font.BOLD, 34));
+        emailTextRyan.setBackground(white);
+        emailTextRyan.setForeground(blue);
+        ryanairPanel.add(emailTextRyan);
+
+        phoneRyan.setBounds(180, 440, 550, 100);
+        phoneRyan.setFont(new Font("Serif", Font.BOLD, 34));
+        phoneRyan.setForeground(Color.blue);
+        ryanairPanel.add(phoneRyan);
+
+        phoneTextRyan.setBounds(320, 461, 350, 60);
+        phoneTextRyan.setFont(new Font("Serif", Font.BOLD, 34));
+        phoneTextRyan.setBackground(white);
+        phoneTextRyan.setForeground(blue);
+        ryanairPanel.add(phoneTextRyan);
+
+        companyRyanMessage.setBounds(180, 540, 550, 100);
+        companyRyanMessage.setFont(new Font("Serif", Font.BOLD, 27));
+        companyRyanMessage.setForeground(Color.blue);
+        ryanairPanel.add(companyRyanMessage);
+
+        ryanTicketButton.setBounds(900, 355, 300, 110 );
+        ryanTicketButton.setText("Order your Ryanair Ticket!");
+        ryanTicketButton.setFont(new Font("Serif", Font.BOLD, 21));
+        ryanairPanel.add(ryanTicketButton);
+
+
+        ryanTicketButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String emailR = emailTextRyan.getText();
+
+                JOptionPane.showMessageDialog(null, "Thank you for flying with us!We sent you a confirmation via e-mail:" + emailR + "!");
+                return;
+            }
+        });
+
+
         ryanairFrame.setVisible(true);
     }
 
     public void setBarcelonaTicket3(){
-        barcelonaTicket3.setBounds(470, 25, 550, 100);
+        barcelonaTicket3.setBounds(470, 25, 750, 100);
         barcelonaTicket3.setFont(new Font("Serif", Font.BOLD, 43));
         barcelonaTicket3.setForeground(Color.blue);
         ryanairPanel.add(barcelonaTicket3);
     }
 
     public void setLondonTicket2(){
-        londonTicket2.setBounds(470, 25, 550, 100);
+        londonTicket2.setBounds(470, 25, 750, 100);
         londonTicket2.setFont(new Font("Serif", Font.BOLD, 43));
         londonTicket2.setForeground(Color.blue);
         ryanairPanel.add(londonTicket2);
     }
 
     public void setManchesterTicket2(){
-        manchesterTicket2.setBounds(470, 25, 550, 100);
+        manchesterTicket2.setBounds(470, 25, 750, 100);
         manchesterTicket2.setFont(new Font("Serif", Font.BOLD, 43));
         manchesterTicket2.setForeground(Color.blue);
         ryanairPanel.add(manchesterTicket2);
